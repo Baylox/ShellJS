84579a7c207e6ed3eae72429b2e35407cee5c2a1
Baylospawn <microsoftbaylo@laposte.net>
2025-03-22 20:12:34 +0100

    refactor REPL input handling to use recursive prompt function

--- app/main.js
+++ app/main.js
@@ -5,21 +5,17 @@ const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
-// Afficher le prompt initial
-rl.prompt();
-
-// Gestion de l'entrée utilisateur
-rl.on("line", (command) => {
-  if (command.trim() === "exit") {
-      rl.close();
-  } else {
-      console.log(`${command}: command not found`);
-      rl.prompt();
-  }
-});
 
 // Gérer la fermeture du REPL proprement
 rl.question("$ ", (answer) => {
   console.log(`${answer}: command not found`);
   rl.close();
 });
+
+function prompt() {
+    rl.question("$ ", (answer) => {
+      console.log(`${answer}: command not found`);
+      prompt(); // Recursively call the function to keep the loop going
+    });
+  }
+  prompt();
